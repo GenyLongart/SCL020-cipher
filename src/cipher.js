1da43e561818f49c2d3ff4b7f724567cd283d44f
genesis <gnsslr@gmail.com>
2022-04-11 11:05:31 -0400

    proyecto con funcionabilidad

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,37 @@
 const cipher = {
-  // ...
+  encode: function (texto, desplazamiento){
+    let resultado= "";
+    const abecedario= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    desplazamiento = (desplazamiento % 26 + 26) % 26;
+
+    if (texto){
+        for (let i=0; i<texto.length; i++){
+            if (abecedario.indexOf(texto[i]) !=-1){
+                let posicion = ((abecedario.indexOf(texto[i])+desplazamiento)%26);
+                resultado += abecedario[posicion];
+
+            }
+            else
+            resultado += texto[i];
+            
+        }
+    
+    } 
+    return resultado;
+
+},
+
+decode: function (texto, desplazamiento){
+  if (!texto)
+  return "";
+const abecedario= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+desplazamiento = (desplazamiento % 26 - 26) % 26;
+return texto.replace(/[A-Z]/ig, c=> abecedario[(abecedario.indexOf(c)-desplazamiento)%26]);
+
+}
+
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+    document.getElementById("cifrar").addEventListener("click", function(e){
+        e.preventDefault()
+        let texto = (document.getElementById("mensaje").value).toUpperCase();
+        let desplazamiento = document.getElementById("desplazamiento").value;
+        document.getElementById("mensaje2").value = cipher.encode(texto, desplazamiento);
+    });
+
+    document.getElementById("descifrar").addEventListener("click", function(e){
+        e.preventDefault()
+        let textoCifrado = document.getElementById("mensaje2").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;
+        document.getElementById("mensaje").value = cipher.decode(textoCifrado, desplazamiento);
+    });
+
+
+
+
+
